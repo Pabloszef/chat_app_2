4fd6f70c243a240359faf16f6d967c950dcbabb8
unknown <pabloszef2115@gmail.com>
2025-08-11 22:26:06 +0200

    Finish Login and Sign up pages - UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
+import Login from "./pages/login/Login.jsx";
+import SignUp from "./pages/signup/SignUp.jsx";
+
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-
+        <SignUp/>
     </div>
   )
 }
