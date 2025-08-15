de5e72ecc613b556b95262bb4bcda9c9f1a7ae60
Pablo <pawel.zaremba013@gmail.com>
2025-08-15 18:33:37 +0200

    Finish Auth Implementation

--- frontend/src/components/Sidebar/LogoutButton.jsx
+++ frontend/src/components/Sidebar/LogoutButton.jsx
@@ -1,9 +1,19 @@
 import {BiLogOut} from "react-icons/bi";
+import useLogout from "../../hooks/useLogout.js";
 
 const LogoutButton = () => {
+    const {loading, logout} = useLogout();
+
     return (
         <div className="mt-auto">
-            <BiLogOut className="size-6 text-white cursor-pointer" />
+            {!loading ? (
+                <BiLogOut
+                    className="size-6 text-white cursor-pointer"
+                    onClick={logout}
+                />
+            ) : (
+                <span className="loading loading-spinner"></span>
+            )}
         </div>
     )
 }
